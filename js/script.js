28929e24e04b8f9dd64aaaf52bf13d5d4972dc2b
ahussain33 <113541906+ahussain33@users.noreply.github.com>
2024-04-02 18:44:42 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,1 @@
-function myFunction() {
-$(document).ready(function(){
-  $('.testimonials').bxSlider({
-    auto: true,
-    pause: 6000, //time on each review
-    mode: 'fade', // or slide
-    infiniteLoop: true,
-    controls: false, // true for arrows
-    slideMargin: 0
-  });
-});
-}
-window.myFunction = myFunction;
+
